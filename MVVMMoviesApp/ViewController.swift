34412ce967f851a07c1a23287f98d582ce419ce4
PutriRahmadewi <putri.rahmadewi@gpi.id>
2023-03-19 23:02:27 +0700

    Initial Commit

---
+++ MVVMMoviesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMMoviesApp
+//
+//  Created by Putri Rahmadewi on 19/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
